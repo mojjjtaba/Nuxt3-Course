a27c66e615fc2ae7baa930c30473ac1afc768771
Mojtaba <smo.hayati@gmail.com>
2024-05-03 17:54:26 +0800

    Pages & Routing - Introduction

---
+++ pages/contact.tsx
@@ -0,0 +1,13 @@
+// .tsx file format
+export default defineNuxtComponent({
+    render() {
+        return <h1>Contact component .tsx file</h1>
+    }
+})
+
+// .ts file format
+// export default defineNuxtComponent({
+//     render() {
+//         return h('h1', 'Contact component .ts file')
+//     }
+// })
