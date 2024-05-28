7ba057841532d9d923ac8cd9280ea9a93a8fed9f
Mojtaba <smo.hayati@gmail.com>
2024-05-28 19:44:06 +0800

    Revert "nuxt link"
    This reverts commit c32670d433c5668131480547a2af9468c44d34bf.

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,11 @@
 <template>
     <div>
         <h1>About page</h1>
-        <button @click="goTo">Go To</button>
     </div>
 </template>
 
 <script setup>
-console.log(window.history)
-async function goTo() {
-    await navigateTo('/', {
-        replace: true
-    })
-}
+// definePageMeta({
+//     layout: false
+// })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,5 @@
 <template>
     <div>
         <h1>Landing page</h1>
-        <button @click="goTo">Go To</button>
     </div>
 </template>
-
-<script setup>
-async function goTo() {
-    // await navigateTo('/about')
-    // await navigateTo('/about', {
-    //     redirectCode: 307
-    // })
-    // await navigateTo({path: '/about'})
-    // await navigateTo({
-    //     name: "about",
-    //     query: {
-    //         color: "red"
-    //     }
-    // })
-    // await navigateTo('https://nuxt.com', {external: true})
-    // await navigateTo('https://nuxt.com', {
-    //     open: {
-    //         target: "_blank",
-    //         windowFeatures: {
-    //             width: 600,
-    //             height: 600,
-    //             popup: true
-    //         }
-    //     }
-    // })
-
-    await navigateTo('/about', {
-        replace: true
-    })
-}
-
-console.log(window.history)
-</script>
