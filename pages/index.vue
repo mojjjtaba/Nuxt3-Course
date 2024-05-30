6fca1514a873a7ae57a0072753248affe507d0b2
Mojtaba <smo.hayati@gmail.com>
2024-05-30 20:44:10 +0800

    components introduction

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,11 @@
 <template>
     <div>
         <h1>Landing page</h1>
-        <button @click="goTo">Go To</button>
+        <AppFooter />
+        <Button />
     </div>
 </template>
 
 <script setup>
-const router = useRouter()
-console.log(router)
-async function goTo() {
-//    console.log(router.currentRoute.value)
-    // router.back()
-    // console.log(router.getRoutes())
-    console.log(router.hasRoute('portfolio'))
-}
+import {AppFooter, Button} from "#components"
 </script>
