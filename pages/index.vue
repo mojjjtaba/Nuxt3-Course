d4afcf94d52ef97ca92b65579e63bcc277f52f12
Mojtaba <smo.hayati@gmail.com>
2024-06-11 01:15:08 +0800

    useRequestURL composable

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
     <div>
         <h1>Landing page</h1>
-        <div>
-            <label :for="uniqueId">Name</label>
-            <input type="text" :id="uniqueId">
-        </div>
+        {{ url.port }}
     </div>
 </template>
 
 <script setup>
-const uniqueId = useId()
-console.log(uniqueId)
+const url = useRequestURL()
+console.log(window?.location)
 </script>
