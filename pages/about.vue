dd80dd3c2a6245c024b31b2413aae2f0e315cd8d
Mojtaba <smo.hayati@gmail.com>
2024-06-29 19:31:37 +0800

    setPageLayout helper function

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <h1>About page</h1>
+        <button @click="changeLayout">Change Layout</button>
     </div>
 </template>
 
 <script setup>
-
+function changeLayout() {
+    setPageLayout('admin')
+}
 </script>
