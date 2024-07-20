20e92e43c9e59046e9806a4982edd705967b9206
Mojtaba <smo.hayati@gmail.com>
2024-07-21 01:18:56 +0800

    Public Directory

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div>
         <!-- <NuxtLayout name="admin"> -->
             <h1>Landing page</h1>
+            <img src="/favicon.ico" alt="">
         <!-- </NuxtLayout> -->
     </div>
 </template>
