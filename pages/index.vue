97fb291fc5f383257494c4de9dd3d87ddb1454fa
Mojtaba <smo.hayati@gmail.com>
2024-07-21 18:12:50 +0800

    Assets Directory

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <!-- <NuxtLayout name="admin"> -->
             <h1>Landing page</h1>
             <img src="/favicon.ico" alt="">
+            <img src="~/assets/nuxt.png" alt="">
         <!-- </NuxtLayout> -->
     </div>
 </template>
