ab8e53dd11521f6a5fa3f6cf4843ba70970bd76e
Mojtaba <smo.hayati@gmail.com>
2024-08-24 22:23:35 +0800

    External Stylesheets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css', 'animate.css']
+  css: ['~/assets/css/main.css'],
+  // app: {
+  //   head: {
+  //     link: [
+  //       {
+  //         rel: 'stylesheet',
+  //         href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+  //       }
+  //     ]
+  //   }
+  // }
 })
